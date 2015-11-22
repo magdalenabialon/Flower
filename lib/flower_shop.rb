ef06628999d286d5ca0e3110a7f3a09ec7451df6
mitulmanish <mitul.manish@gmail.com>
2015-11-22 21:20:00 +1100

    Reconfigured the algirithm and fixed a bug

--- lib/flower_shop.rb
+++ lib/flower_shop.rb
@@ -17,11 +17,14 @@ the quantity entered by the user.
   end
 
   def calculator
-    # calculating all the different combinations by which a certain quantity can be acheived
+    # calculating all the different combinations by which a certain quantity can be achieved
     keys = price_list.keys.sort
     singles(keys)
     doubles(keys)
-    triplets(keys) if keys.length > 2
+    if keys.length > 2
+      triplets(keys)
+      revised(keys)
+    end
     price_holder.sort.to_h
     #bundle_size_holder.sort.to_h
     #[price_holder.sort.to_h,bundle_size_holder.sort.to_h]
@@ -41,19 +44,47 @@ the quantity entered by the user.
       content = []
       if ((quantity-highest*counter) > middle) and ((quantity-highest*counter)%middle)%lowest == 0 and (quantity-(highest*counter) > 0) and ((quantity-highest*counter)%middle) > 0
         price = (price_list[highest] * counter)
-        content << "#{counter}  X  #{highest} $ #{price_list[highest] * counter}"
+        content << "#{counter}  X  #{highest} $ #{(price_list[highest] * counter).round(ROUNDING_CONST)}"
         bundle_size = counter
         revised_quantity = quantity-(highest*counter)
         price += (price_list[middle] * (revised_quantity/middle))
         bundle_size += (revised_quantity/middle)
-        content << "#{(revised_quantity/middle)} X  #{middle} $ #{price_list[middle] * (revised_quantity/middle)}"
+        content << "#{(revised_quantity/middle)} X  #{middle} $ #{(price_list[middle] * (revised_quantity/middle)).round(ROUNDING_CONST)}"
         revised_quantity = revised_quantity%middle
         price += (price_list[lowest] * (revised_quantity/lowest))
         bundle_size += (revised_quantity/lowest)
-        content << "#{revised_quantity/lowest} X #{lowest} $ #{price_list[lowest] * (revised_quantity/lowest)}"
+        content << "#{revised_quantity/lowest} X #{lowest} $ #{(price_list[lowest] * (revised_quantity/lowest)).round(ROUNDING_CONST)}"
         price_bundle_updater price,content,bundle_size
       end
       counter-=1
+    end
+  end
+
+
+  def revised (keys)
+    lowest,middle,highest = keys[0],keys[1],keys[2]
+    counter = 1
+    while (highest * counter) < quantity and (quantity % (highest * counter) != 0)
+      revised_quantity = quantity - (highest * counter)
+      new_counter = 1
+      while (middle * new_counter) < revised_quantity and (revised_quantity % (middle * new_counter) != 0)
+        if ((revised_quantity - (middle * new_counter)) % lowest) == 0 and ((revised_quantity - (middle * new_counter)) > lowest)
+          new_quantity = revised_quantity-(middle * new_counter)
+          content = []
+          price = (price_list[highest] * counter)
+          bundle_size = counter
+          content << "#{counter}  X  #{highest} $.#{(price_list[highest] * counter).round(ROUNDING_CONST)}"
+          price += (price_list[middle] * (new_counter))
+          bundle_size += new_counter
+          content << "#{new_counter} X  #{middle} $.#{(price_list[middle] * (new_counter)).round(ROUNDING_CONST)}"
+          price += (price_list[lowest] * (new_quantity/lowest))
+          bundle_size += new_quantity/lowest
+          content << "#{new_quantity/lowest} X #{lowest} $.#{(price_list[lowest] * (new_quantity/lowest)).round(ROUNDING_CONST)}"
+          price_bundle_updater price,content,bundle_size
+        end
+        new_counter += 1
+      end
+      counter += 1
     end
   end
 
@@ -69,11 +100,11 @@ the quantity entered by the user.
         if ((quantity-highest*counter) % lowest == 0) and (quantity-highest*counter) > 0 and quantity > highest
           price = (price_list[highest] * counter)
           bundle_size = counter
-          content << "#{counter}  X  #{highest}  $ #{price_list[highest] * counter}"
+          content << "#{counter}  X  #{highest}  $ #{(price_list[highest] * counter).round(ROUNDING_CONST)}"
           revised_quantity = quantity-(highest*counter)
           price += (price_list[lowest] * (revised_quantity/lowest))
           bundle_size += (revised_quantity/lowest)
-          content << "#{revised_quantity/lowest}  X  #{lowest}  $ #{price_list[lowest] * (revised_quantity/lowest)}"
+          content << "#{revised_quantity/lowest}  X  #{lowest}  $ #{(price_list[lowest] * (revised_quantity/lowest)).round(ROUNDING_CONST)}"
           price_bundle_updater price,content,bundle_size
         end
         counter-=1
@@ -88,13 +119,13 @@ the quantity entered by the user.
       if quantity % key == 0
         price = (price_list[key] * (quantity/key))
         bundle_size = (quantity/key)
-        content << "#{quantity/key}  X   #{key}  $ #{price_list[key] * (quantity/key)}"
+        content << "#{quantity/key}  X   #{key}  $ #{(price_list[key] * (quantity/key)).round(ROUNDING_CONST)}"
         price_bundle_updater price,content,bundle_size
       end
     end
   end
 
-  def price_bundle_updater (price,content,bundle_size)
+  def price_bundle_updater (price,content,bundle_size=0)
     # update the pricing details
     price_holder[price.round(ROUNDING_CONST)] = content
     bundle_size_holder[price.round(ROUNDING_CONST)] = bundle_size
